ca3b0d5d2f17efadea490497d317df2da37477ad
PatrickNg <patrickngzh@outlook.com>
2020-06-25 17:45:17 +0800

    Add the object

---
+++ src/com/patrickng/learning/object/ObjectDemo.groovy
@@ -0,0 +1,8 @@
+package com.patrickng.learning.object
+
+/**
+ * @author patrickng
+ * @date 25/6/2020 5:38 PM
+ */
+def person = new Person(name: "Patrick", age: 24)
+println "The name is ${person.name}, the age is ${person.getAge()}"
---
+++ src/com/patrickng/learning/object/Person.groovy
@@ -0,0 +1,14 @@
+package com.patrickng.learning.object
+
+/**
+ * @author patrickng* @date 25/6/2020 5:33 PM
+ */
+class Person {
+    String name
+
+    int age
+
+    def increaseAge(int years) {
+        age += years
+    }
+}
